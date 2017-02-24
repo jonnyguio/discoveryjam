11b9223f8164a889c03a7e65cb4c5299a559c2e3
Joao Guio <joaoluisguio@gmail.com>
2017-02-24 19:41:39 -0300

    Fixes #3 + Luz se move pelo mouse

---
+++ Luz.gd
@@ -0,0 +1,13 @@
+extends Node2D
+
+# class member variables go here, for example:
+# var a = 2
+# var b = "textvar"
+
+func _ready():
+	# Called every time the node is added to the scene.
+	# Initialization here
+	set_fixed_process(true)
+
+func _fixed_process(delta):
+	set_global_pos(get_global_mouse_pos())
