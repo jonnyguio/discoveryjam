b7959f0d0ed31ac922df6f8ddbece477fa282f24
Joao Guio <joaoluisguio@gmail.com>
2017-02-24 18:47:39 -0300

    !  Melhorando o player andando. + Adicionado chão

--- Player.gd
+++ Player.gd
@@ -5,6 +5,8 @@ extends KinematicBody2D
 # var b = "textvar"
 
 var velocity = Vector2(0, 0)
+var speedBase = 150
+var gravity = 11
 
 func _ready():
 	# Called every time the node is added to the scene.
@@ -13,20 +15,28 @@ func _ready():
 	set_process_input(true)
 	
 func _fixed_process(delta):
-	move(velocity)
-	velocity.y += 2 * delta
+	velocity.y += gravity
+	
+	var motion = velocity * delta
+	motion = move(motion)
+	
+	if (is_colliding()):
+		var n = get_collision_normal()
+		motion = n.slide(motion)
+		velocity = n.slide(velocity)
+		move(motion)
 	
 func _input(event):
 	if (event.is_action_pressed("ui_left")):
-		velocity.x += -1
+		velocity.x += -1 * speedBase
 	if (event.is_action_pressed("ui_right")):
-		velocity.x += 1
+		velocity.x += 1 * speedBase
 	if (event.is_action_pressed("ui_up")):
-		velocity.y = -2
+		velocity.y = -3 * speedBase
 	if (event.is_action_released("ui_left")):
-		velocity.x += 1
+		velocity.x += 1 * speedBase
 	if (event.is_action_released("ui_right")):
-		velocity.x += -1
+		velocity.x += -1 * speedBase
 		
 	
 	
