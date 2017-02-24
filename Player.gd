34da05e01aca49bf7ee740552e289a42776c1d22
Joao Guio <joaoluisguio@gmail.com>
2017-02-24 20:28:56 -0300

    Fazendo animação do player

--- Player.gd
+++ Player.gd
@@ -7,6 +7,7 @@ extends KinematicBody2D
 var velocity = Vector2(0, 0)
 var speedBase = 150
 var gravity = 11
+var jumps = 1
 
 func _ready():
 	# Called every time the node is added to the scene.
@@ -25,18 +26,29 @@ func _fixed_process(delta):
 		motion = n.slide(motion)
 		velocity = n.slide(velocity)
 		move(motion)
+		if (n == Vector2(0, -1)):
+			jumps = 1
+		
+	if (velocity.x != 0):
+		get_node("AnimationPlayer").set_active(true)
+	else:
+		get_node("AnimationPlayer").set_active(false)
 	
 func _input(event):
 	if (event.is_action_pressed("ui_left")):
 		velocity.x += -1 * speedBase
+		set_scale(Vector2(-1, 1))
 	if (event.is_action_pressed("ui_right")):
 		velocity.x += 1 * speedBase
+		set_scale(Vector2(1, 1))
 	if (event.is_action_pressed("ui_up")):
-		velocity.y = -3 * speedBase
+		if (jumps > 0):
+			velocity.y = -3 * speedBase
+			jumps -= 1
 	if (event.is_action_released("ui_left")):
 		velocity.x += 1 * speedBase
 	if (event.is_action_released("ui_right")):
 		velocity.x += -1 * speedBase
-		
+
 	
 	
