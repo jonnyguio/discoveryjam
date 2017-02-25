94964f0c6bce0f943e21704c258258247e8c149c
rilpires <rilpires@gmail.com>
2017-02-24 22:18:57 -0300

    luz acho q ta funcionando

--- Player.gd
+++ Player.gd
@@ -37,6 +37,7 @@ func _fixed_process(delta):
 		get_node("AnimationPlayer").set_active(false)
 		
 	updateControl()
+	
 	if (get_scale().x > 0):
 		get_node("RayCast2D").set_cast_to(Vector2(10, 0))
 		get_node("RayCast2D").set_pos(Vector2(30, 0))
