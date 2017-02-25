300da1e1e98667035195689b45b0bd1c169bc69d
Joao Guio <joaoluisguio@gmail.com>
2017-02-24 22:11:44 -0300

    Corrigindo merge e trocando Raycast2D

--- Player.gd
+++ Player.gd
@@ -37,14 +37,8 @@ func _fixed_process(delta):
 		get_node("AnimationPlayer").set_active(false)
 		
 	updateControl()
-	if (get_scale().x > 0):
-		get_node("RayCast2D").set_cast_to(Vector2(10, 0))
-		get_node("RayCast2D").set_pos(Vector2(30, 0))
-	else:
-		get_node("RayCast2D").set_cast_to(Vector2(-10, 0))
-		get_node("RayCast2D").set_pos(Vector2(-30, 0))
-	var collider = get_node("RayCast2D").get_collider()
-	if (collider and is_colliding()):
+	var collider = get_node("Sprite").get_node("RayCast2D").get_collider()
+	if (collider and is_colliding() and collider.get_type() == "KinematicBody2D"):
 		collider.moveBlock(velocity, delta)
 	
 func updateControl():
